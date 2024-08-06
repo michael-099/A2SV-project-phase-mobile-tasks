cbb461f43f9928401520d43c3da938537cb1a8a8
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-06 15:45:12 +0300

    Add a new product View all products View a single product Edit a product (update name, description, price) Delete a product
    functions

---
+++ on-boarding/product-2/main.dart
@@ -0,0 +1,19 @@
+import "./product_manager_class.dart";
+import 'product_class.dart';
+
+void main() {
+  ProductManager productManager = ProductManager();
+  Product product1 = Product(name: "top", discription: "water", price: 25);
+  Product product2 = Product(name: "yes", discription: "water", price: 25);
+  Product product3 = Product(name: "gold", discription: "water", price: 25);
+
+  productManager.addProduct(product1);
+  productManager.addProduct(product2);
+  productManager.addProduct(product3);
+
+  productManager.viewAllProduct();
+  productManager.deleteProduct("gold");
+  productManager.viewAllProduct();
+  productManager.editProduct();
+  productManager.viewAllProduct();
+}
--- on-boarding/product-2/product_manager_class.dart
+++ on-boarding/product-2/product_manager_class.dart
@@ -1,24 +1,122 @@
-// Add a new product
-// View all products
-// View a single product
-// Edit a product (update name, description, price)
-// Delete a product
+import "dart:io";
 
-class Product_Manager{
-  addProduct(){
+import '../product-2/product_class.dart';
 
-  }
-  viewAllProduct(){
-    
-  }
-  viewSingleProduct(){
-    
-  }
-  editProduct(){
-    
-  }
-  deleteProduct(){
-    
+class ProductManager {
+  List<Product> products = [];
+
+  void addProduct(Product product) {
+    products.add(product);
+    print("product successfuly added");
   }
 
+  void viewAllProduct() {
+    if (products.isEmpty) {
+      print("empty Product");
+    } else {
+      print("product" + "|" + "discription" + "|" + "price");
+      for (int i = 0; i < products.length; i++) {
+        print(products[i].name +
+            "|" +
+            products[i].discription +
+            "|" +
+            products[i].price.toString());
+      }
+    }
+  }
+
+  void viewSingleProduct(String product) {
+    for (int i = 0; i < products.length; i++) {
+      if (product == products[i].name) {
+        print(products[i].name +
+            "|" +
+            products[i].discription +
+            "|" +
+            products[i].price.toString());
+
+        break;
+      }
+    }
+  }
+
+  editProduct() {
+    print("name of the product to edit");
+    String? product = stdin.readLineSync();
+    if (product == null) {
+      return;
+    }
+
+    for (int i = 0; i < products.length; i++) {
+      if (product == products[i].name) {
+        print("press 1 to edit name ");
+        print("press 2 to edit discription ");
+        print("press 3 to edit price ");
+        print("press 4 to edit all ");
+        String? input = stdin.readLineSync();
+        if (input == null) {
+          print("no input detacted");
+        } else {
+          if (input == "1") {
+            print("enter the new name ");
+            String? inputName = stdin.readLineSync();
+            if (inputName != null) {
+              products[i].name = inputName;
+            }
+            print("null input");
+          }
+
+          if (input == "2") {
+            print("enter the new discreption ");
+            String? inputDiscription = stdin.readLineSync();
+            if (inputDiscription != null) {
+              products[i].discription = inputDiscription;
+            }
+            print("null input");
+          }
+
+          if (input == "3") {
+            print(" enter the new price ");
+            String? inputPrice = stdin.readLineSync();
+            if (inputPrice != null) {
+              double newPrice = double.parse(inputPrice);
+              products[i].price = newPrice;
+            }
+          }
+
+          if (input == "4") {
+            print("enter the new name  ");
+            String? inputName = stdin.readLineSync();
+            if (inputName != null) {
+              products[i].name = inputName;
+            }
+
+            print("enter the new discreption ");
+            String? inputDiscription = stdin.readLineSync();
+            if (inputDiscription != null) {
+              products[i].discription = inputDiscription;
+            }
+
+            print(" enter the new price ");
+            String? inputPrice = stdin.readLineSync();
+            if (inputPrice != null) {
+              double newPrice = double.parse(inputPrice);
+              products[i].price = newPrice;
+            }
+          }
+        }
+      }
+    }
+    print("not Found");
+  }
+
+  void deleteProduct(String product) {
+    for (int i = 0; i < products.length; i++) {
+      if (product == products[i].name) {
+        print(products[i].name);
+        products.remove(products[i]);
+        break;
+      }
+    }
+    print("not Found");
+  }
 }
