89edad198d1bf8acc9c9b3e84f39e067cd6f1c4c
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-06 11:59:32 +0300

    product manger class template

---
+++ on-boarding/product-2/product_manager_class.dart
@@ -0,0 +1,24 @@
+// Add a new product
+// View all products
+// View a single product
+// Edit a product (update name, description, price)
+// Delete a product
+
+class Product_Manager{
+  addProduct(){
+
+  }
+  viewAllProduct(){
+    
+  }
+  viewSingleProduct(){
+    
+  }
+  editProduct(){
+    
+  }
+  deleteProduct(){
+    
+  }
+
+}
