696901857be51c9725551ccbac522dd8275a0c00
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-08 14:59:51 +0300

    add floating action button

--- ecommerce_app/lib/screens/home_page.dart
+++ ecommerce_app/lib/screens/home_page.dart
@@ -8,67 +8,50 @@ class HomePage extends StatelessWidget {
       home: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const TopBar(),
-              
-              ProductCard(
-                productName: "Derby Leather Shoes",
-                price: "\$200",
-                label: "Men's shoe",
-                rating: "4.5",
-                height: 300,
-                width: 420,
-              ),
-              
-              ProductCard(
-                productName: "Derby Leather Shoes",
-                price: "\$200",
-                label: "Men's shoe",
-                rating: "4.5",
-                height: 300,
-                width: 420,
-              ),
-              
-              ProductCard(
-                productName: "Derby Leather Shoes",
-                price: "\$200",
-                label: "Men's shoe",
-                rating: "4.5",
-                height: 300,
-                width: 420,
-              ),
-              
-              ProductCard(
-                productName: "Derby Leather Shoes",
-                price: "\$200",
-                label: "Men's shoe",
-                rating: "4.5",
-                height: 300,
-                width: 420,
-              ),
-              
-
-              Align(
-                alignment: Alignment.bottomRight,
-                child: FloatingActionButton(
-                  onPressed: () {},
-                  backgroundColor: const Color.fromARGB(255, 0, 125, 227),
-                  child: const Text(
-                    "+",
-                    style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
-                    ),
-                  ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const TopBar(),
+                ProductCard(
+                  productName: "Derby Leather Shoes",
+                  price: "\$200",
+                  label: "Men's shoe",
+                  rating: "4.5",
+                  height: 300,
+                  width: 420,
                 ),
-              )
-
-            ],
+                ProductCard(
+                  productName: "Derby Leather Shoes",
+                  price: "\$200",
+                  label: "Men's shoe",
+                  rating: "4.5",
+                  height: 300,
+                  width: 420,
+                ),
+                ProductCard(
+                  productName: "Derby Leather Shoes",
+                  price: "\$200",
+                  label: "Men's shoe",
+                  rating: "4.5",
+                  height: 300,
+                  width: 420,
+                ),
+              ],
+            ),
           ),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
-
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: const Color.fromARGB(255, 0, 125, 227),
+          child: const Text(
+            "+",
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      
       ),
     );
   }
