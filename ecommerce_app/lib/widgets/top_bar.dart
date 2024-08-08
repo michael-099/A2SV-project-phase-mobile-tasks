303cc88b2464c6a6eb026f213550386bfdfd654e
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-08 14:26:25 +0300

    topbar skeleton

--- ecommerce_app/lib/widgets/top_bar.dart
+++ ecommerce_app/lib/widgets/top_bar.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TopBar extends StatelessWidget {
   const TopBar({super.key});
@@ -7,19 +9,52 @@ class TopBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Card(
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Row(
             children: [
               CircleAvatar(),
+              SizedBox(width: 10),
               Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text("time-time-time"),
-                  Text("name name "),
+                  Text(
+                    "time-time-time",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      // backgroundColor: Colors.amber,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black38),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Hello, ",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          // backgroundColor: Colors.blue,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black38),
+                      ),
+                      Text(
+                        "Michael",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black87),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ],
           ),
-          Icon(Icons.notification_add_outlined),
+          Icon(
+            Icons.notifications_active_outlined,
+          ),
         ],
       ),
     );
