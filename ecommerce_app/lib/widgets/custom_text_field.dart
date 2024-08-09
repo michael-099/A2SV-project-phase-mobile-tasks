55e526d7fe1b91ad6b43323ce73c4d9ab40a9bdd
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-09 09:59:21 +0300

    controller for custom text field

--- ecommerce_app/lib/widgets/custom_text_field.dart
+++ ecommerce_app/lib/widgets/custom_text_field.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatefulWidget {
-  String label;
-  CustomTextField({super.key, required this.label});
+  final String label;
+  final TextEditingController controller;
+  const CustomTextField(
+      {super.key, required this.label, required this.controller});
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
@@ -11,21 +13,22 @@ class CustomTextField extends StatefulWidget {
 class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         children: [
           Align(
             alignment: Alignment.topLeft,
-            child: Text("input"),
+            child: Text(widget.label),
           ),
           TextField(
-            decoration: InputDecoration(
+            controller: widget.controller,
+            decoration: const InputDecoration(
                 focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.blue),
                 ),
                 border: OutlineInputBorder()),
-            style: TextStyle(),
+            style: const TextStyle(),
           ),
         ],
       ),
