0fa1cbfba1bcd6ec56f7aaff144fff58c59e2ef5
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-09 11:36:48 +0300

    buttons for the dicription page

--- ecommerce_app/lib/screens/add_product_page.dart
+++ ecommerce_app/lib/screens/add_product_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-
 
 import '../widgets/custom_text_field.dart';
 
@@ -17,7 +17,6 @@ class _AddProductPageState extends State<AddProductPage> {
   final TextEditingController _priceController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,18 +25,77 @@ class _AddProductPageState extends State<AddProductPage> {
           leading: const Icon(Icons.arrow_back_ios),
           title: const Text("Search Product"),
         ),
-        body: Form(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 40,
-              ),
-              CustomTextField(label: "Name",controller:_nameController ,maxLines: 1,minLines: 1,),
-              CustomTextField(label: "Category",controller:_categoryController ,maxLines: 1,minLines: 1,),
-              CustomTextField(label: "price",controller:_priceController ,maxLines: 1,minLines: 1,),
-              CustomTextField(label: "Description",controller:_descriptionController ,maxLines: 5,minLines: 1,),
-
-            ],
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                CustomTextField(
+                  label: "Name",
+                  controller: _nameController,
+                  maxLines: 1,
+                  minLines: 1,
+                ),
+                CustomTextField(
+                  label: "Category",
+                  controller: _categoryController,
+                  maxLines: 1,
+                  minLines: 1,
+                ),
+                CustomTextField(
+                  label: "price",
+                  controller: _priceController,
+                  maxLines: 1,
+                  minLines: 1,
+                ),
+                CustomTextField(
+                  label: "Description",
+                  controller: _descriptionController,
+                  maxLines: 5,
+                  minLines: 1,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: FilledButton(
+                    onPressed: () {},
+                    child: const Text("Add"),
+                    style: FilledButton.styleFrom(
+                      side: const BorderSide(color: Colors.blue),
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: const Text("Delete"),
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: Colors.red),
+                      foregroundColor: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- ecommerce_app/lib/widgets/custom_text_field.dart
+++ ecommerce_app/lib/widgets/custom_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class CustomTextField extends StatefulWidget {
   final String label;
@@ -20,27 +21,30 @@ class CustomTextField extends StatefulWidget {
 class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.topLeft,
-            child: Text(widget.label),
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            widget.label,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blueGrey,
+            ),
           ),
-          TextField(
-            minLines: widget.minLines,
-            maxLines: widget.maxLines,
-            controller: widget.controller,
-            decoration: const InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blue),
-                ),
-                border: OutlineInputBorder()),
-            style: const TextStyle(),
-          ),
-        ],
-      ),
+        ),
+        TextField(
+          minLines: widget.minLines,
+          maxLines: widget.maxLines,
+          controller: widget.controller,
+          decoration: const InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+              border: OutlineInputBorder()),
+          style: const TextStyle(),
+        ),
+      ],
     );
   }
 }
