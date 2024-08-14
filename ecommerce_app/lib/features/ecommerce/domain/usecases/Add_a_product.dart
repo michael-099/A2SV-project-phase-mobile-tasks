efd7e1bc1e62df3de39913c8a1eae81ad724c166
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-14 11:23:12 +0300

    test for add a product

---
+++ ecommerce_app/lib/features/ecommerce/domain/usecases/Add_a_product.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/error/failure.dart';
+import '../entites/product.dart';
+import '../repositories/product_repository.dart';
+
+class AddProduct {
+  final ProductRepository repository;
+
+  AddProduct(this.repository);
+
+  Future<Either<Failure, Product>> execute({required Product product}) 
+  async {
+    return await repository.addProduct(product);
+  }
+}
---
+++ ecommerce_app/test/features/ecommerce/domain/usecases/add_a_product_test.dart
@@ -0,0 +1,60 @@
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce_app/core/error/failure.dart';
+import 'package:ecommerce_app/features/ecommerce/domain/entites/product.dart';
+import 'package:ecommerce_app/features/ecommerce/domain/repositories/product_repository.dart';
+import 'package:ecommerce_app/features/ecommerce/domain/usecases/add_a_product.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'add_a_product_test.mocks.dart'; 
+
+
+@GenerateMocks([
+  ProductRepository
+])
+void main() {
+  late AddProduct usecase;
+  late MockProductRepository mockProductRepository;
+
+  setUp(() {
+    mockProductRepository = MockProductRepository();
+    usecase = AddProduct(mockProductRepository);
+  });
+
+  final product = Product(id: '02', name: 'name', description: 'description', price: 120, url: 'url');
+
+  test(
+    'should add new product to the repository and return the created entity.',
+    () async {
+      // Mocking a successful product addition
+      when(mockProductRepository.addProduct(product))
+          .thenAnswer((_) async => Right(product)); 
+      
+      // Executing the use case
+      final result = await usecase.execute(product: product);
+
+      // Assertions
+      expect(result, Right(product));
+      verify(mockProductRepository.addProduct(product));
+      verifyNoMoreInteractions(mockProductRepository);
+    },
+  );
+
+  test(
+    'should return a Failure when the repository fails',
+    () async {
+      // Mocking a failure in the repository
+      when(mockProductRepository.addProduct(product))
+          .thenAnswer((_) async => const Left(NetworkFailure("Failed to add product")));
+    
+      // Executing the use case
+      final result = await usecase.execute(product: product);
+
+      // Assertions
+      expect(result, const Left(NetworkFailure("Failed to add product")));
+      verify(mockProductRepository.addProduct(product));
+      verifyNoMoreInteractions(mockProductRepository);
+    },
+  );
+}
