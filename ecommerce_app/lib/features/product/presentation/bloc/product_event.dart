f6ef32c7dbd955742df724307009ae4c6061d384
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-20 10:04:48 +0300

    -m

--- ecommerce_app/lib/features/product/presentation/bloc/product_event.dart
+++ ecommerce_app/lib/features/product/presentation/bloc/product_event.dart
@@ -6,3 +6,40 @@ sealed class ProductEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+class LoadAllProductsEvent extends ProductEvent {
+  const LoadAllProductsEvent();
+}
+
+class GetSpecificProductEvent extends ProductEvent {
+  final String id;
+
+  const GetSpecificProductEvent({required this.id});
+  @override
+  List<Object> get props => [id];
+}
+
+class UpdateProductEvent extends ProductEvent {
+  final Product product;
+
+  const UpdateProductEvent({required this.product});
+
+  @override
+  List<Object> get props => [product];
+}
+
+class DeleteProductEvent extends ProductEvent {
+  final String id;
+
+  const DeleteProductEvent({required this.id});
+
+  @override
+  List<Object> get props => [id];
+}
+
+class AddProductEvent extends ProductEvent {
+  final Product product;
+
+  const AddProductEvent({required this.product});
+  @override
+  List<Object> get props => [product];
+}
